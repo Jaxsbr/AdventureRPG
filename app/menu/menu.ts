2682d4118eb6972611aafe32c3b50c9676474bfc
Unknown <jbrink386@gmail.com>
2017-08-26 23:59:53 +0200

    Replace manual animation with animation implementation

--- app/menu/menu.ts
+++ app/menu/menu.ts
@@ -7,11 +7,12 @@ class Menu {
     playImage: HTMLImageElement;
 
     playButtonRect: Rectangle;
+    playAnimation: Animation;
 
-    shrink: boolean;
-    tick: number;
-    ellapsed: number;
-    helpText: string;
+    // shrink: boolean;
+    // tick: number;
+    // ellapsed: number;
+    // helpText: string;
 
 
     // TODO:
@@ -27,29 +28,32 @@ class Menu {
         this.playImage = this.game.assetManager.getImage('play');
 
         this.playButtonRect = new Rectangle(0, 0, 200, 110);
-        this.shrink = false;
-        this.tick = 0.8;
-        this.ellapsed = 0;
+        this.playAnimation = new Animation(this.playImage, 0.08, 340, 78, 6, this.renderWorker);
+
+        // this.shrink = false;
+        // this.tick = 0.8;
+        // this.ellapsed = 0;
     }
 
     update(delta: number) {
-        this.updatePulse(delta);
-        this.updateSize();
+        //this.updatePulse(delta);
+        //this.updateSize();
         this.updateButtonPosition();
+        this.playAnimation.update(this.playButtonRect, delta);
     }
 
     updatePulse(delta: number) {
-        this.ellapsed += delta;
-        if (this.ellapsed >= this.tick) {
-            this.ellapsed = 0;
-            this.shrink = !this.shrink;
-        }        
+        // this.ellapsed += delta;
+        // if (this.ellapsed >= this.tick) {
+        //     this.ellapsed = 0;
+        //     this.shrink = !this.shrink;
+        // }        
     }
 
     updateSize() {
-        let crement = this.shrink ? -0.8 : 0.8;
-        this.playButtonRect.width += crement;
-        this.playButtonRect.height += crement;
+        // let crement = this.shrink ? -0.8 : 0.8;
+        // this.playButtonRect.width += crement;
+        // this.playButtonRect.height += crement;
     }
 
     updateButtonPosition() {
@@ -63,8 +67,12 @@ class Menu {
         this.renderWorker.clear(context, this.game.screenBounds);        
         this.renderWorker.renderImage(context, this.backgroundImage, 
             this.game.screenBounds.x, this.game.screenBounds.y, this.game.screenBounds.width, this.game.screenBounds.height);
-        this.renderWorker.renderImage(context, this.playImage,
-            this.playButtonRect.x, this.playButtonRect.y, this.playButtonRect.width, this.playButtonRect.height);
+
+        this.playAnimation.draw(context);
+
+
+        // this.renderWorker.renderImage(context, this.playImage,
+        //     this.playButtonRect.x, this.playButtonRect.y, this.playButtonRect.width, this.playButtonRect.height);
 
         //this.renderWorker.renderText(context, 'Menu State', 100, 100);
         //this.renderWorker.renderText(context, this.helpText, 100, 150);
