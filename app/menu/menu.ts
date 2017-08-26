51531bcf6c1d00740f913c7c3d45d3ccd77b0295
Unknown <jbrink386@gmail.com>
2017-08-27 00:00:58 +0200

    Code clean up

--- app/menu/menu.ts
+++ app/menu/menu.ts
@@ -9,15 +9,6 @@ class Menu {
     playButtonRect: Rectangle;
     playAnimation: Animation;
 
-    // shrink: boolean;
-    // tick: number;
-    // ellapsed: number;
-    // helpText: string;
-
-
-    // TODO:
-    // Replace pulsing menu button calculation simple with animation.
-
 
     constructor(game: Game, stateManger: StateManager, renderWorker: RenderWorker) {
         this.game = game;
@@ -29,31 +20,11 @@ class Menu {
 
         this.playButtonRect = new Rectangle(0, 0, 200, 110);
         this.playAnimation = new Animation(this.playImage, 0.08, 340, 78, 6, this.renderWorker);
-
-        // this.shrink = false;
-        // this.tick = 0.8;
-        // this.ellapsed = 0;
     }
 
     update(delta: number) {
-        //this.updatePulse(delta);
-        //this.updateSize();
         this.updateButtonPosition();
         this.playAnimation.update(this.playButtonRect, delta);
-    }
-
-    updatePulse(delta: number) {
-        // this.ellapsed += delta;
-        // if (this.ellapsed >= this.tick) {
-        //     this.ellapsed = 0;
-        //     this.shrink = !this.shrink;
-        // }        
-    }
-
-    updateSize() {
-        // let crement = this.shrink ? -0.8 : 0.8;
-        // this.playButtonRect.width += crement;
-        // this.playButtonRect.height += crement;
     }
 
     updateButtonPosition() {
@@ -69,13 +40,6 @@ class Menu {
             this.game.screenBounds.x, this.game.screenBounds.y, this.game.screenBounds.width, this.game.screenBounds.height);
 
         this.playAnimation.draw(context);
-
-
-        // this.renderWorker.renderImage(context, this.playImage,
-        //     this.playButtonRect.x, this.playButtonRect.y, this.playButtonRect.width, this.playButtonRect.height);
-
-        //this.renderWorker.renderText(context, 'Menu State', 100, 100);
-        //this.renderWorker.renderText(context, this.helpText, 100, 150);
     }
 
     tap(point: Point) {
